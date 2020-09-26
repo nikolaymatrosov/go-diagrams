3804c0009b1265c1d34ac0a63850787c749d5037
Nikolay Matrosov <nikthespirit@yandex-team.ru>
2020-09-26 10:30:49 +0300

    Fix readme

--- _examples/app/main.go
+++ _examples/app/main.go
@@ -4,7 +4,8 @@ import (
 	"log"
 
 	"github.com/blushft/go-diagrams/diagram"
-	"github.com/blushft/go-diagrams/nodes/gcp"
+	"github.com/blushft/go-diagrams/nodes/apps"
+	"github.com/blushft/go-diagrams/nodes/yc"
 )
 
 func main() {
@@ -13,25 +14,25 @@ func main() {
 		log.Fatal(err)
 	}
 
-	dns := gcp.Network.Dns(diagram.NodeLabel("DNS"))
-	lb := gcp.Network.LoadBalancing(diagram.NodeLabel("NLB"))
-	cache := gcp.Database.Memorystore(diagram.NodeLabel("Cache"))
-	db := gcp.Database.Sql(diagram.NodeLabel("Database"))
+	user := apps.Client.User(diagram.NodeLabel("User"))
+	lb := yc.Infrastructure.LoadBalancer(diagram.NodeLabel("LB"))
+	cache := yc.Datastorage.ManagedRedis(diagram.NodeLabel("Cache"))
+	db := yc.Datastorage.ManagedPostgresql(diagram.NodeLabel("Database"))
 
-	dc := diagram.NewGroup("GCP")
+	dc := diagram.NewGroup("YandexCloud").Label("YandexCloud")
 	dc.NewGroup("services").
 		Label("Service Layer").
 		Add(
-			gcp.Compute.ComputeEngine(diagram.NodeLabel("Server 1")),
-			gcp.Compute.ComputeEngine(diagram.NodeLabel("Server 2")),
-			gcp.Compute.ComputeEngine(diagram.NodeLabel("Server 3")),
+			yc.Infrastructure.Compute(diagram.NodeLabel("Server 1")),
+			yc.Infrastructure.Compute(diagram.NodeLabel("Server 2")),
+			yc.Infrastructure.Compute(diagram.NodeLabel("Server 3")),
 		).
 		ConnectAllFrom(lb.ID(), diagram.Forward()).
 		ConnectAllTo(cache.ID(), diagram.Forward())
 
 	dc.NewGroup("data").Label("Data Layer").Add(cache, db).Connect(cache, db)
 
-	d.Connect(dns, lb, diagram.Forward()).Group(dc)
+	d.Connect(user, lb, diagram.Forward()).Group(dc)
 
 	if err := d.Render(); err != nil {
 		log.Fatal(err)
